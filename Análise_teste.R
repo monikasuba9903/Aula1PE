3970cdb0299edbfebcf46a9f0735bf6b32bc775b
monikasuba9903 <monika_suba@id.uff.br>
2025-11-03 10:06:48 -0300

    Inclusão do cálculo da correlação

--- Análise_teste.R
+++ Análise_teste.R
@@ -1,6 +1,12 @@
 
-#Criando um vetor
+#Criando um vetor x
 x <- c(4, 2, 66, 75, 23)
 
 #Calculando a média de x
 mean(x = x, na.rm = TRUE)
+
+#Criando um vetor y
+y <- c(43, 53, 77, 11, 97)
+
+#Calculando a correlação entre x e y
+cor(x = x, y = y)
